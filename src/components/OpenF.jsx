356c4fd759051288981c4daedc595c08a6067105
George losaberidze <you@example.com>
2023-11-19 17:57:34 +0400

    DSxz

--- src/components/OpenF.jsx
+++ src/components/OpenF.jsx
@@ -1,21 +1,45 @@
-import React, { useState } from "react";
+import React, { useState} from "react";
 import audioG from "../assets/G.wav";
 import audioD from "../assets/D.wav";
 import audioA from "../assets/A.wav";
 
 function Openfret() {
-  const [currentAudio, setCurrentAudio] = useState(null);
+    const [currentAudio, setCurrentAudio] = useState(null);
 
-  const playAudio = (audio) => {
-    if (currentAudio) {
-      currentAudio.pause(); // Pause the currently playing audio
-    }
-
-    const newAudio = new Audio(audio);
-    setCurrentAudio(newAudio); // Set the new audio as the current audio
-    newAudio.play();
-  };
-
+    const playAudio = async (audio) => {
+      if (currentAudio) {
+        decreaseVolumeCurrentAudio();
+      }
+  
+      const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+      const audioSource = audioContext.createBufferSource();
+      const gainNode = audioContext.createGain();
+      audioSource.connect(gainNode);
+      gainNode.connect(audioContext.destination);
+  
+      try {
+        const response = await fetch(audio);
+        const arrayBuffer = await response.arrayBuffer();
+        const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
+        audioSource.buffer = audioBuffer;
+        audioSource.start(0);
+  
+        setCurrentAudio({ source: audioSource, gainNode });
+      } catch (error) {
+        console.error("Error loading audio:", error);
+      }
+    };
+  
+    const decreaseVolumeCurrentAudio = () => {
+      if (currentAudio) {
+        const { gainNode } = currentAudio;
+        gainNode.gain.linearRampToValueAtTime(0, gainNode.context.currentTime + 0.5);
+        setTimeout(() => {
+          currentAudio.source.stop();
+        }, 500); // Stop the current audio after decreasing volume
+      }
+    };
+  
   return (
     <li className="first-fret">
       <div onClick={() => playAudio(audioG)} data-note="g" className="first-fret-note">
